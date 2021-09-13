ca57eb0f6e15f04ac9b0be6cb8fcfe412fd26b94
qlhan <qlhan@incarcloud.com>
2021-09-13 14:59:51 +0800

    0.1.0-SNAPSHOT init proj

---
+++ stl-single-backend/src/main/java/com/example/stl/SingleApp.java
@@ -0,0 +1,23 @@
+package com.example.stl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试单体应用
+ *
+ * @author Aaric, created on 2021-09-13T14:49.
+ * @version 0.1.0-SNAPSHOT
+ */
+@SpringBootApplication
+public class SingleApp {
+
+    /**
+     * main
+     *
+     * @param args custom inputs
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SingleApp.class, args);
+    }
+}
