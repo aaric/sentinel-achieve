3831d3438d09653ea8138c72c05000bd6e2bba90
qlhan <qlhan@incarcloud.com>
2021-09-13 17:04:35 +0800

    0.2.0-SNAPSHOT SphU.entry("hello")

--- stl-single-backend/src/main/java/com/example/stl/api/test/impl/TestController.java
+++ stl-single-backend/src/main/java/com/example/stl/api/test/impl/TestController.java
@@ -1,11 +1,20 @@
 package com.example.stl.api.test.impl;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.example.stl.api.test.TestApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 简单测试模块API接口控制器
@@ -18,10 +27,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/test/test")
 public class TestController implements TestApi {
 
+    @PostConstruct
+    public void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule qpsRule = new FlowRule()
+                .setGrade(RuleConstant.FLOW_GRADE_QPS)
+                // limit qps to 2
+                .setCount(2);
+        qpsRule.setResource("hello");
+        rules.add(qpsRule);
+        FlowRuleManager.loadRules(rules);
+    }
+
     @Override
     @GetMapping("/hello")
-    @SentinelResource("hello")
+    //@SentinelResource("hello")
     public String hello() {
-        return "hello world";
+        try (Entry entry = SphU.entry("hello")) {
+            return "hello sentinel";
+
+        } catch (BlockException e) {
+            log.error("hello exception", e);
+            return "hello error";
+        }
     }
 }
