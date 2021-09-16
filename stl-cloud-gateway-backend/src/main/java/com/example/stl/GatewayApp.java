5900fb5eecf734cf6b87c2a842648ac6467806cc
qlhan <qlhan@incarcloud.com>
2021-09-16 17:08:49 +0800

    0.3.0-SNAPSHOT add zuul gw

--- stl-cloud-gateway-backend/src/main/java/com/example/stl/GatewayApp.java
+++ stl-cloud-gateway-backend/src/main/java/com/example/stl/GatewayApp.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * gateway微服务应用
@@ -14,6 +15,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
+@EnableZuulProxy
 public class GatewayApp {
 
     /**
